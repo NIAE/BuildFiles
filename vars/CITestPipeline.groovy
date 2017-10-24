12d1a5d3b11b6756d5c8faae0a51ba2ab8ac75e3
NIAE <niapplicationsengineering@gmail.com>
2017-10-24 16:28:57 -0500

    Bad at Brackets

--- vars/CITestPipeline.groovy
+++ vars/CITestPipeline.groovy
@@ -1,8 +1,12 @@
-#!/usr/bin/env groovy
+##!/usr/bin/env groovy
 
-//CITestPipeline(utfPaths, lvVersion)
+//note: this script assumes that it will be invoked from another script after that script has defined the necessary parameters
 
-def call(utfPaths,lvVersion){
+//These are: utfPath, vipbPath, lvVersion
+
+//This script further assumes that Jenkins is configured (via the Pipeline Shared Libraries plugin) to implicitly include https://github.com/LabVIEW-DCAF/buildsystem
+
+def call(utfPaths,vipbPaths,lvVersion){
 
 switch(lvVersion){  //This is to abstract out the different Jenkinsfile conventions of setting version to 14.0 instead of 2014.
   case "14.0":
@@ -20,43 +24,30 @@ switch(lvVersion){  //This is to abstract out the different Jenkinsfile conventi
 }
 
 def continueBuild
-  
-	node(lvVersion){
-	{
+  node(lvVersion){
         echo 'Starting build...'
-		
-		
       stage ('Pre-Clean'){
         preClean()
       }
-	  
-	  
       stage ('SCM_Checkout'){
         echo 'Attempting to get source from repo...'
         timeout(time: 5, unit: 'MINUTES'){
           checkout scm
         }
       }
-      
-	  
-	  // If this change is a pull request and the DIFFING_PIC_REPO variable is set on the jenkins master, diff vis.
+      // If this change is a pull request and the DIFFING_PIC_REPO variable is set on the jenkins master, diff vis.
       if (env.CHANGE_ID && env.DIFFING_PIC_REPO) {
         stage ('Diff VIs'){
           lvDiff(lvVersion)
         }
       }
-	  
-	  
       stage ('Check Preconditions for Build'){
         continueBuild=checkCommits()
       }
-	  
-	  
     if(continueBuild){
         stage ('Temp Directories'){
           bat 'mkdir build_temp'
         }
-		
         stage ('UTF'){
           utfPaths.each{utfPath->
             echo 'UTF path: '+utfPath
@@ -65,11 +56,15 @@ def continueBuild
             }
           }
         }
-     }
-  }
-       
-	   stage ('Post-Clean'){
+
+      //stage ('SCM commit'){
+      //  vipbPaths.each{vipbPath->
+      //    commitPackageToGit(vipbPath)
+      //  }
+     // }
+        stage ('Post-Clean'){
           postClean()
         }    
-	}
+    }
+  }
 }
